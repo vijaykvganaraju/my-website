ee38473b6362292e59248d6b645e025b828f4e74
VJmix <vaishnavvarma.ganaraju@gmail.com>
2020-04-11 20:29:14 +0530

    Optimized for SEO

--- assets/scripts/common.js
+++ assets/scripts/common.js
@@ -1,6 +1,6 @@
 function viewMenu() {
 
-    console.log('click')
+    let menu = ''; 
     let navButton = document.querySelector('.nav-button').innerHTML;
     let menuState = document.querySelector('.menu').style.display;
 
@@ -30,4 +30,23 @@ window.addEventListener('resize', () => {
     document.querySelector('.nav-button').innerText = 'menu';
 
 });
+
+function redirectSocial(media) {
+    console.log(media);
+    let link = null;
+    if (media === 'li') {
+        link = 'https://www.linkedin.com/in/vijay-ganaraju-2a0982126';
+    } else if (media === 'tw') {
+        link = 'https://twitter.com/vijaykvganaraju';
+    } else if (media === 'in') {
+        link = 'https://www.instagram.com/vijaykvganaraju';
+    } else if (media === 'di') {
+        link = 'https://discord.gg/C4HSFYN';
+    } else if (media === 'yt') {
+        link = 'https://www.youtube.com/user/Tejaswi88';
+    }
+
+    window.open(link, '_blank', 'noopener');
+
+}
 
