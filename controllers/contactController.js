ec7d81810b5aa82864285378aaaf8dbf1f3cc1b9
VJmix <vaishnavvarma.ganaraju@gmail.com>
2020-04-20 22:17:50 +0530

    Completed contact page with token verification

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const nodemailer = require('nodemailer');
-
+const request = require('request');
 // const process = require('./../process');
 
 // Path of views directory
@@ -27,17 +27,30 @@ exports.getContactPage = (req, res, next) => {
 };
 
 exports.contactMe = (req, res, next) => {
-    if (req.body['g-recaptcha-response'].length > 0 && (req.body.firstName.length > 0 && req.body.lastName.length > 0 && req.body.email.length > 0 && req.body.subject.length > 0 && (req.body.message.length > 0 && req.body.message.length < 1000))) {
-        mailOptions.from = `"${req.body.firstName} ${req.body.lastName}" <${ req.body.email }>`;
-        mailOptions.subject = req.body.subject;
-        mailOptions.text = req.body.message;
-        tranporter.sendMail(mailOptions, err => {
+    let gCaptchaResponse = req.body['g-recaptcha-response'];
+    let validationToken = false;
+    request.post({ url: 'https://www.google.com/recaptcha/api/siteverify',
+        form: { secret: '6Lf16-oUAAAAAJC0w9_9ITvNT_US1FZ5JlZW7xHk',  response: gCaptchaResponse }},
+        (err, httpRes, body) => {
             if(err) {
+                console.error(err);
+                return;
+            }
+            validationToken = JSON.parse(body).success;
+            if (validationToken === true && (req.body.firstName.length > 0 && req.body.lastName.length > 0 && req.body.email.length > 0 && req.body.subject.length > 0 && (req.body.message.length > 0 && req.body.message.length < 1000))) {
+                mailOptions.from = `"${req.body.firstName} ${req.body.lastName}" <${req.body.email}>`;
+                mailOptions.subject = req.body.subject;
+                mailOptions.text = req.body.message;
+                tranporter.sendMail(mailOptions, (err, info) => {
+                    if (err) {
+                        res.sendFile('contact_error.html', { root: viewsPath });
+                    }
+                });
+                res.sendFile('contact_success.html', { root: viewsPath });
+            } else {
                 res.sendFile('contact_error.html', { root: viewsPath });
             }
+            
         });
-        res.sendFile('contact_success.html', { root: viewsPath });
-    } else {
-        res.sendFile('contact_error.html', { root: viewsPath });
-    }
+        
 };
