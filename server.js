c538e5d9873ba93d57ce38812206e4ba91d9d36f
VJmix <vaishnavvarma.ganaraju@gmail.com>
2020-04-24 18:12:19 +0530

    Removed https

--- server.js
+++ server.js
@@ -1,12 +1,5 @@
-var https = require('https');
 var http = require('http');
-var fs = require('fs');
 var app = require('./app');
 
-var options = {
-    key: fs.readFileSync('key.pem'),
-    cert: fs.readFileSync('cert.pem')
-};
 
 http.createServer(app).listen(process.env.PORT || 9000 );
-https.createServer(options, app).listen(443);
