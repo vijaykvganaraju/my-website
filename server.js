db0f8640471f4c8d12066bb01a32915bd8bd4498
VJmix <vaishnavvarma.ganaraju@gmail.com>
2020-04-24 18:05:04 +0530

    Rectified server file

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 var https = require('https');
 var fs = require('fs');
 var app = require('./app');
+const port = process.env.PORT || 3000;
 
 var options = {
     key: fs.readFileSync('key.pem'),
     cert: fs.readFileSync('cert.pem')
 };
 
-https.createServer(options, app).listen(process.env.PORT || 3000);
+https.createServer(options, app).listen(port, () => console.log(`Listening on Port ${port}`));
